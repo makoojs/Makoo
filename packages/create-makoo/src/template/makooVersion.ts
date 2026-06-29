1e91110171e1f73ee083d08d726cbc6b7afe48d4
FlowingInk <1561317465@qq.com>
2026-06-29 23:01:06 +0800

    fix: make makoo core a peer dependency

--- packages/create-makoo/src/template/makooVersion.ts
+++ packages/create-makoo/src/template/makooVersion.ts
@@ -1,7 +1,7 @@
 export const recommendedMakooVersions = {
-	core: '^0.1.0',
-	cli: '^0.1.1',
-	vue: '^0.1.0',
-	react: '^0.1.0',
-	'create-makoo': '^0.1.2'
+	core: '^0.2.0',
+	cli: '^0.3.0',
+	vue: '^0.1.1',
+	react: '^0.1.1',
+	'create-makoo': '^0.1.3'
 } as const satisfies Record<string, string>;
