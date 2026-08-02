bcd99053c1117ab66016b99579f4450ae9b75afe
FlowingInk <1561317465@qq.com>
2026-08-02 15:58:24 +0800

    chore:update changeset config and alignment makoo version

--- packages/create-makoo/src/template/makooVersion.ts
+++ packages/create-makoo/src/template/makooVersion.ts
@@ -1,7 +1,7 @@
 export const recommendedMakooVersions = {
-	core: '^0.2.2',
-	cli: '^0.3.2',
-	vue: '^0.1.2',
-	react: '^0.1.2',
-	'create-makoo': '^0.1.6'
+	core: '^0.3.0',
+	cli: '^0.4.0',
+	vue: '^0.1.3',
+	react: '^0.1.3',
+	'create-makoo': '^0.1.7'
 } as const satisfies Record<string, string>;
