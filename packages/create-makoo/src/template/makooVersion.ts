aea3fe55daa62dc5ef9d4aad0a2306fa10d4fa50
FlowingInk <1561317465@qq.com>
2026-08-05 22:50:58 +0800

    chore:update makoo version

--- packages/create-makoo/src/template/makooVersion.ts
+++ packages/create-makoo/src/template/makooVersion.ts
@@ -3,5 +3,5 @@ export const recommendedMakooVersions = {
 	cli: '^0.4.1',
 	vue: '^0.1.3',
 	react: '^0.1.3',
-	'create-makoo': '^0.1.7'
+	'create-makoo': '^0.1.8'
 } as const satisfies Record<string, string>;
